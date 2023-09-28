fcabdac02236abc1eb92ef883fdc303718ce64c3
Ryan Li <jijizaza999@gmail.com>
2023-09-28 16:34:51 -0700

    fixed

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,100 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/***/ (() => {
+/***/ "./src/car.js":
+/*!********************!*\
+  !*** ./src/car.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log(\"Hello World\");\n\n// TODO\n\n\n//# sourceURL=webpack://es6_modules/./src/index.js?");
+  eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Car: () => (/* binding */ Car)\n/* harmony export */ });\n\r\n\r\nclass Car {\r\n    constructor(id, make, model, year) {\r\n      this.id = id;\r\n      this.make = make;\r\n      this.model = model;\r\n      this.year = year;\r\n    }\r\n  }\r\n  \n\n//# sourceURL=webpack://es6_modules/./src/car.js?");
 
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
-/******/ 	
-/******/ })()
-;
+  /***/ }),
+  
+  /***/ "./src/index.js":
+  /*!**********************!*\
+    !*** ./src/index.js ***!
+    \**********************/
+  /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+  
+  eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _wishlist_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./wishlist.js */ \"./src/wishlist.js\");\n\r\n\r\n\r\nlet makeInput = document.getElementById(\"makeInput\");\r\nlet form = document.getElementById(\"submitForm\");\r\nlet modelInput = document.getElementById(\"modelInput\");\r\nlet yearInput = document.getElementById(\"yearInput\");\r\nlet makeDisplay = document.getElementById(\"car-make\");\r\nlet modelDisplay = document.getElementById(\"car-model\");\r\nlet yearDisplay = document.getElementById(\"car-year\");\r\nlet removeBtn = document.querySelector(\".removeBtn\");\r\nlet wishlistUl = document.querySelector(\"#wishListContainer ul\");\r\n\r\n\r\nlet wishlist = new _wishlist_js__WEBPACK_IMPORTED_MODULE_0__.WishList();\r\n\r\nfunction showCarDetails(car) {\r\n  makeDisplay.textContent = car.make;\r\n  modelDisplay.textContent = car.model;\r\n  yearDisplay.textContent = car.year;\r\n  removeBtn.disabled = false;\r\n  removeBtn.setAttribute(\"data-carId\", car.id);\r\n}\r\n\r\nfunction updateDOMList() {\r\n  wishlistUl.innerHTML = \"\";\r\n  wishlist.list.forEach((car) => {\r\n    const li = document.createElement(\"li\");\r\n    li.textContent = `${car.make} ${car.model}`;\r\n    li.addEventListener(\"click\", () => showCarDetails(car));\r\n    wishlistUl.appendChild(li);\r\n  });\r\n}\r\n  \r\nfunction addCar(event) {\r\n  \r\n  event.preventDefault();\r\n  \r\n  let make = makeInput.value;\r\n  let model = modelInput.value;\r\n  let year = yearInput.value;\r\n  \r\n  wishlist.add(make, model, year);\r\n  \r\n  \r\n  updateDOMList();\r\n}\r\n\r\nfunction removeCar() {\r\n  let carId = Number(removeBtn.getAttribute(\"data-carId\"));\r\n  wishlist.remove(carId);\r\n  \r\n  updateDOMList();\r\n  \r\n  makeDisplay.textContent = \"\";\r\n  modelDisplay.textContent = \"\";\r\n  yearDisplay.textContent = \"\";\r\n  removeBtn.disabled = true;\r\n}\r\n\r\n\r\nform.addEventListener(\"submit\", addCar);\r\nremoveBtn.addEventListener(\"click\", removeCar);\n\n//# sourceURL=webpack://es6_modules/./src/index.js?");
+  
+  /***/ }),
+  
+  /***/ "./src/wishlist.js":
+  /*!*************************!*\
+    !*** ./src/wishlist.js ***!
+    \*************************/
+  /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+  
+  eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   WishList: () => (/* binding */ WishList)\n/* harmony export */ });\n/* harmony import */ var _car_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./car.js */ \"./src/car.js\");\n\r\n\r\n\r\n\r\nclass WishList {\r\n    constructor() {\r\n      this.list = [];\r\n      this.nextId = 0;\r\n    }\r\n  \r\n    add(make, model, year) {\r\n      const car = new _car_js__WEBPACK_IMPORTED_MODULE_0__.Car(++this.nextId, make, model, year);\r\n      this.list.push(car);\r\n    }\r\n\r\n  //   remove(carId) {\r\n  //   const index = this.list.findIndex((car) => car.id === carId);\r\n\r\n  //   if (index !== -1) {\r\n  //     this.list.splice(index, 1);\r\n  //   }\r\n  // }\r\n  remove(carId) {\r\n    this.list = this.list.filter((car) => car.id != carId);\r\n  }\r\n  }\n\n//# sourceURL=webpack://es6_modules/./src/wishlist.js?");
+  
+  /***/ })
+  
+  /******/ 	});
+  /************************************************************************/
+  /******/ 	// The module cache
+  /******/ 	var __webpack_module_cache__ = {};
+  /******/ 	
+  /******/ 	// The require function
+  /******/ 	function __webpack_require__(moduleId) {
+  /******/ 		// Check if module is in cache
+  /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+  /******/ 		if (cachedModule !== undefined) {
+  /******/ 			return cachedModule.exports;
+  /******/ 		}
+  /******/ 		// Create a new module (and put it into the cache)
+  /******/ 		var module = __webpack_module_cache__[moduleId] = {
+  /******/ 			// no module.id needed
+  /******/ 			// no module.loaded needed
+  /******/ 			exports: {}
+  /******/ 		};
+  /******/ 	
+  /******/ 		// Execute the module function
+  /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+  /******/ 	
+  /******/ 		// Return the exports of the module
+  /******/ 		return module.exports;
+  /******/ 	}
+  /******/ 	
+  /************************************************************************/
+  /******/ 	/* webpack/runtime/define property getters */
+  /******/ 	(() => {
+  /******/ 		// define getter functions for harmony exports
+  /******/ 		__webpack_require__.d = (exports, definition) => {
+  /******/ 			for(var key in definition) {
+  /******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+  /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+  /******/ 				}
+  /******/ 			}
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+  /******/ 	(() => {
+  /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/make namespace object */
+  /******/ 	(() => {
+  /******/ 		// define __esModule on exports
+  /******/ 		__webpack_require__.r = (exports) => {
+  /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+  /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+  /******/ 			}
+  /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /************************************************************************/
+  /******/ 	
+  /******/ 	// startup
+  /******/ 	// Load entry module and return exports
+  /******/ 	// This entry module can't be inlined because the eval devtool is used.
+  /******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+  /******/ 	
+  /******/ })()
+  ;
--- src/index.js
+++ src/index.js
@@ -1,85 +1,62 @@
-import  WishList  from "./wishlist.js";
+import { WishList } from "./wishlist.js";
 
-console.log("index.js start");
 
-// TODO
-// Select elements
 const form = document.getElementById("submitForm");
-const makeInput = document.getElementById("makeInput");
 const modelInput = document.getElementById("modelInput");
 const yearInput = document.getElementById("yearInput");
-
-const makeP = document.getElementById("car-make");
-const modelP = document.getElementById("car-model");
-const yearP = document.getElementById("car-year");
-
-const removeBtn = document.getElementById("removeBtn");
-const submitBtn = document.getElementById("submitBtn");
-
-removeBtn.addEventListener("click", removeCar, false);
-submitBtn.addEventListener("click", addCar, false);
-
-const listEle = document.getElementsByTagName("ul")[0];
+const makeDisplay = document.getElementById("car-make");
+const modelDisplay = document.getElementById("car-model");
+const yearDisplay = document.getElementById("car-year");
+const removeBtn = document.querySelector(".removeBtn");
+const wishlistUl = document.querySelector("#wishListContainer ul");
 
 
-// create new instance for WishList
-let wishList = new WishList();
+const wishlist = new WishList();
 
-function showCarDetails (car) {
-    makeP.textContent = car.make;
-    modelP.textContent = car.model;
-    yearP.textContent = car.year;
-
-    // enable remove button
-    removeBtn.disabled = false;
-    removeBtn.setAttribute("data-carId", car.id);
+function showCarDetails(car) {
+  makeDisplay.textContent = car.make;
+  modelDisplay.textContent = car.model;
+  yearDisplay.textContent = car.year;
+  removeBtn.disabled = false;
+  removeBtn.setAttribute("data-carId", car.id);
 }
 
-function updateDOMList () {
-    // clear contents of 'ul'
-    listEle.innerHTML = "";
-
-    // iterate over each car in wishlist.list
-    wishList.list.forEach((car) => {
-        // create a new 'li' that displays the car's make and model
-        const li = document.createElement("li");
-        li.textContent = `${car.make} ${car.model}`;
-
-        // add a click event listener to the 'li'
-        li.addEventListener("click", () => showCarDetails(car));
-
-        // append each 'li' to the 'ul'
-        listEle.appendChild(li);
-    });
-
+function updateDOMList() {
+  wishlistUl.innerHTML = "";
+  wishlist.list.forEach((car) => {
+    const li = document.createElement("li");
+    li.textContent = `${car.make} ${car.model}`;
+    li.addEventListener("click", () => showCarDetails(car));
+    wishlistUl.appendChild(li);
+  });
+}
+  
+function addCar(event) {
+  
+  event.preventDefault();
+  
+  let make = makeInput.value;
+  let model = modelInput.value;
+  let year = yearInput.value;
+  
+  wishlist.add(make, model, year);
+  
+  
+  updateDOMList();
 }
 
-function addCar (event) {
-    event.preventDefault();
-    if ((makeInput.value != "") && (modelInput.value != "") && (yearInput.value != "")) {
-        console.log("Passing the " + makeInput.value);
-        wishList.add(makeInput.value, modelInput.value, yearInput.value);
-        updateDOMList();
-
-        // QOL, clear the input fields
-        makeInput.value = "";
-        modelInput.value = "";
-        yearInput.value = "";
-
-    }
-
-    else {
-        console.log("Input cannot be blank");
-    }
+function removeCar() {
+  let carId = Number(removeBtn.getAttribute("data-carId"));
+  wishlist.remove(carId);
+  
+  updateDOMList();
+  
+  makeDisplay.textContent = "";
+  modelDisplay.textContent = "";
+  yearDisplay.textContent = "";
+  removeBtn.disabled = true;
 }
 
 
-function removeCar () {
-    let carId = Number(removeBtn.getAttribute("data-carId"));
-    wishList.remove(carId);
-    updateDOMList();
-    makeP.textContent = "";
-    modelP.textContent = "";
-    yearP.textContent = "";
-    removeBtn.disabled = true;
-}
+form.addEventListener("submit", addCar);
+removeBtn.addEventListener("click", removeCar);
